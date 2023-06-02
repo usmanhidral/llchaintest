2cb528f8ba21b09a38394097d355502a29a7d0a5
usmanhidral <m.usmanhidral@gmail.com>
2023-06-02 22:06:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = ' 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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
