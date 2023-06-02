9d1d93afd8b2918bb1241d6b3d0bb81ffba17874
usmanhidral <m.usmanhidral@gmail.com>
2023-06-02 21:57:00 +0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,66 @@
+#Import os to set API key
+import os
+# Import OpenAI as main LLM service
+from langchain.llms import OpenAI
+# Bring in streamlit for UI/app interface
+import streamlit as st
+
+# Import PDF document loaders...there's other ones as well!
+from langchain.document_loaders import PyPDFLoader
+# Import chroma as the vector store
+from langchain.vectorstores import Chroma
+
+# Import vector store stuffpip install PyPDF2
+
+from langchain.agents.agent_toolkits import (
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo
+)
+
+# Set APIkey for OpenAI Service
+# Can sub this out for other LLM providers
+os.environ['OPENAI_API_KEY'] = ' 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'
+
+# Create instance of OpenAI LLM
+llm = OpenAI(temperature=0.1, verbose=True)
+
+# Create and load PDF Loader
+loader =  PyPDFLoader(r"file.pdf")
+# Split pages from pdf
+pages = loader.load_and_split()
+# Load documents into vector database aka ChromaDB
+store = Chroma.from_documents(pages, collection_name='file')
+
+# Create vectorstore info object - metadata repo?
+vectorstore_info = VectorStoreInfo(
+    name="file_report",
+    description="a file report report as a pdf",
+    vectorstore=store
+)
+# Convert the document store into a langchain toolkit
+toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+
+# Add the toolkit to an end-to-end LC
+agent_executor = create_vectorstore_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True
+)
+st.title('Project')
+# Create a text input box for the user
+prompt = st.text_input('Input your prompt here')
+
+# If the user hits enter
+if prompt:
+    # Then pass the prompt to the LLM
+    response = agent_executor.run(prompt)
+    # ...and write it out to the screen
+    st.write(response)
+
+    # With a streamlit expander
+    with st.expander('Document Similarity Search'):
+        # Find the relevant pages
+        search = store.similarity_search_with_score(prompt)
+        # Write out the first
+        st.write(search[0][0].page_content)
